856f39c0c1bc638e162920b56e969617be6cdd31
Rene <reneleogp0305@gmail.com>
2021-12-21 23:55:00 -0500

    CHANGELOG, MANIFEST

---
+++ pylivecoinwatch/api.py
@@ -0,0 +1,50 @@
+import requests
+import json
+
+from requests.adapters import HTTPAdapter
+# from requests.packages.urllib3.util.retry import Retry
+
+# from .utils import func_args_preprocessing
+
+
+class LiveCoinWatchAPI:
+    base_url = 'https://api.livecoinwatch.com'
+    api_key = '8acf6a03-a29b-4cdd-b08c-58bf2ed72678'
+
+    def __init__(self):
+        self.api_base_url = LiveCoinWatchAPI.base_url
+        self.request_timeout = 120
+        self.session = requests.Session()
+        self.session.mount('http://', HTTPAdapter(max_retries=5))
+        self.headers = {
+            'content-type': 'application/json',
+            'x-api-key': LiveCoinWatchAPI.api_key
+        }
+
+    def set_api(self, user_api_key):
+        self.headers['x-api-key'] = user_api_key
+
+    def __request(self, url, payload):
+        url = self.api_base_url + url
+        response = self.session.post(url, headers=self.headers, data=payload)
+        return (response.json())
+
+    def status(self):
+        url = '/status'
+        payload = {}
+        return self.__request(url, payload)
+
+    def credits(self):
+        url = '/credits'
+        payload = {}
+        return self.__request(url, payload)
+
+    def overview(self, currency):
+        url = '/overview'
+        payload = {'currency': currency}
+        return self.__request(url, payload)
+
+
+lcw = LiveCoinWatchAPI()
+
+print(lcw.credits())
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='pylivecoinwatch',
     version='1.0.0',
     license='MIT',
-    description='Python wrapper around the CoinGecko API',
+    description='Python wrapper around Live Coin Watch API',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     author='Rene Gonzalez',
@@ -12,10 +12,11 @@ setup(
 
     packages=find_packages(),
     install_requires=['requests'],
+    keywords=['crypto', 'crypto bot', 'livecoinwatch'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 2",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
+    ]
 )
