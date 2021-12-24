2f1505a4914c226d232a090246136730677427cb
Rene <reneleogp0305@gmail.com>
2021-12-24 02:50:33 -0500

    HTTPAdapter included

--- pylivecoinwatch/api.py
+++ pylivecoinwatch/api.py
@@ -3,7 +3,7 @@ import requests
 import json
 import warnings
 
-# from requests.adapters import HTTPAdapter
+from requests.adapters import HTTPAdapter
 # from requests.packages.urllib3.util.retry import Retry
 
 # from .utils import func_args_preprocessing
