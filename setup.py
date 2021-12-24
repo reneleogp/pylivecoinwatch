62a86dc9d6946f48c91c551d64a4ea48f612f7a2
Rene <reneleogp0305@gmail.com>
2021-12-24 02:46:16 -0500

    Release 0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pylivecoinwatch',
-    version='0.0.3',
+    version='0.1',
     license='MIT',
     description='Python wrapper around Live Coin Watch API',
     long_description=open('README.md').read() + "\n\n" + open("CHANGELOG.md").read(),
